1536cde6aac289be112c030850159c9f58ec56fe
catalin-enache <catalinenache78@gmail.com>
2015-08-01 13:55:03 +0300

    initial

--- ntk_extensions/management/commands/ntk_reset.py
+++ ntk_extensions/management/commands/ntk_reset.py
@@ -1,22 +1,23 @@
 
 from django.core.management.base import BaseCommand, CommandError
-from django.db import DEFAULT_DB_ALIAS, connections, models
+from django.db import DEFAULT_DB_ALIAS, connections
 from django.conf import settings
-import shutil, os, importlib, inspect
+import shutil, os
 from django.contrib.contenttypes.models import ContentType
 
+
 class Command(BaseCommand):
-    help = 'Resets django database and migrations'
+    help = 'Resets django database and migrations for specified app (or entire project)'
     # python manage.py ntk_reset --help
     # python manage.py help ntk_reset
 
     def add_arguments(self, parser):
-        parser.add_argument('--database', action='store', dest='database', default=DEFAULT_DB_ALIAS, help='Nominates which database to reset')
-        parser.add_argument('--app', action='store', dest='app', default='*', help='Nominates which app to reset')
+        parser.add_argument('app', nargs='?', type=str, help='Nominates which app to reset')
+        parser.add_argument('--database', action='store', default=DEFAULT_DB_ALIAS, help='Nominates which database to reset default: %(default)s')
 
     def handle(self, *args, **options):
         app_to_reset = options.get('app')
-        if app_to_reset != '*' and app_to_reset not in settings.INSTALLED_APPS:
+        if app_to_reset != 'ALL' and app_to_reset not in settings.INSTALLED_APPS:
             print ('%s is not installed' % app_to_reset)
             return
 
@@ -25,10 +26,10 @@ class Command(BaseCommand):
         cursor = connection.cursor()
         tables = connection.introspection.table_names(cursor)
 
-        # remove tables
+        # remove app tables
         try:
             for table in tables:
-                if app_to_reset != '*' and not table.startswith('%s_' % app_to_reset):
+                if app_to_reset != 'ALL' and not table.startswith('%s_' % app_to_reset):
                     continue
                 print('DROP TABLE %s' % (table,))
                 try:
@@ -38,18 +39,18 @@ class Command(BaseCommand):
         except Exception as e:
             print(e)
 
-
-
-        # remove traces
+        # remove app models traces
         # http://www.marinamele.com/how-to-correctly-remove-a-model-or-app-in-django
         # https://fragmentsofcode.wordpress.com/2010/09/21/cleanly-removing-a-django-app/
-        ContentType.objects.filter(app_label=app_to_reset).delete()  # also removes foreign entries in other django tables
-        cursor.execute('DELETE FROM django_migrations WHERE app = %s ', [app_to_reset])
+        if app_to_reset != 'ALL':
+            ContentType.objects.filter(app_label=app_to_reset).delete()  # also removes foreign entries in other django tables
+            cursor.execute('DELETE FROM django_migrations WHERE app = %s ', [app_to_reset])
+
         cursor.close()
 
         # remove migration files
         for app in settings.INSTALLED_APPS:
-            if app_to_reset != '*' and not app == app_to_reset:
+            if app_to_reset != 'ALL' and not app == app_to_reset:
                     continue
             dir_to_look_into = os.path.join(settings.BASE_DIR, app, 'migrations')
             if os.path.isdir(dir_to_look_into):
