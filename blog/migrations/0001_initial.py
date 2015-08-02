f861c79f9561fc1a9dc7a66b288a96493e9f72e1
catalin-enache <catalinenache78@gmail.com>
2015-08-02 15:06:12 +0300

    initial

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=128)),
+                ('body', models.TextField()),
+                ('excerpt', models.TextField(max_length=256)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=32)),
+                ('last_name', models.CharField(max_length=32)),
+                ('email', models.EmailField(max_length=254)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Label',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=32)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='author',
+            field=models.ForeignKey(to='blog.Author'),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='tags',
+            field=models.ManyToManyField(to='blog.Label'),
+        ),
+    ]
